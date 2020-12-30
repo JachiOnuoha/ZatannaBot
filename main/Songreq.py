69031aeb2cb64c098dbfdcecb664dbafe35c2f8f
Jachimike Onuoha <jon226@g.uky.edu>
2020-12-30 00:43:50 -0500

    Completed starter OAuth code for spotify Web API

--- main/Songreq.py
+++ main/Songreq.py
@@ -9,3 +9,20 @@ load_dotenv()
 
 scope_queue = "streaming"
 scope_currPlaying = "user-read-currently-playing"
+
+# This is the SpotipyOAUTH object that will be used to manage authorization and make API calls
+sp = SpotifyOAuth(
+        client_id=os.getenv("SPOTIPY_CLIENT_ID"),
+        client_secret=os.getenv("SPOTIPY_CLIENT_SECRET"),
+        redirect_uri=os.getenv("SPOTIPY_REDIRECT_URI"),
+        state="code",
+        scope=scope_currPlaying)
+
+
+def authorizeSongReq():
+    authCode = sp.get_authorization_code()
+    accessToken = sp.get_access_token(authCode, as_dict=False)
+    print(accessToken)
+
+
+authorizeSongReq()
