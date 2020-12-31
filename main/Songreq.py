6452c269ff355bf19c8006842eefaa1a9e4928fe
Jachimike Onuoha <jon226@g.uky.edu>
2020-12-30 21:14:53 -0500

    Successfully got currently playing song

--- main/Songreq.py
+++ main/Songreq.py
@@ -2,7 +2,7 @@
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 
-# Access to environment vaariables
+# Access to environment variables
 from dotenv import load_dotenv
 import os
 load_dotenv()
@@ -10,8 +10,8 @@ load_dotenv()
 scope_queue = "streaming"
 scope_currPlaying = "user-read-currently-playing"
 
-# This is the SpotipyOAUTH object that will be used to manage authorization and make API calls
-sp = SpotifyOAuth(
+# These are the SpotipyOAUTH object that will be used to manage authorization and make API calls
+sp_1 = SpotifyOAuth(
         client_id=os.getenv("SPOTIPY_CLIENT_ID"),
         client_secret=os.getenv("SPOTIPY_CLIENT_SECRET"),
         redirect_uri=os.getenv("SPOTIPY_REDIRECT_URI"),
@@ -20,9 +20,17 @@ sp = SpotifyOAuth(
 
 
 def authorizeSongReq():
-    authCode = sp.get_authorization_code()
-    accessToken = sp.get_access_token(authCode, as_dict=False)
+    authCode = sp_1.get_authorization_code()
+    accessToken = sp_1.get_access_token(authCode, as_dict=False)
     print(accessToken)
 
 
 authorizeSongReq()
+
+songRequest = spotipy.Spotify(auth_manager=sp_1)
+
+trackData = songRequest.currently_playing(market="US")
+trackName = trackData['item']['name']
+artistName = trackData['item']['artists'][0]['name']
+print("Song: {}".format(trackName))
+print("Artist(s): {}".format(artistName))
